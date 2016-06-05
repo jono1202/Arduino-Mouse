b6a887d2c887354174583f843b6320d67e3cc0d0
jono1202 <jonathan8589@gmail.com>
2016-06-05 11:07:10 -0700

    Add files via upload

---
+++ Arduino_Mouse/Arduino_Mouse.ino
@@ -0,0 +1,28 @@
+
+#include "MotorDriver.h"
+
+MotorDriver motor;
+
+void setup()
+{
+    // initialize
+    motor.begin();
+    motor.stop(1);
+    motor.stop(0);
+}
+
+void loop()
+{
+    motor.speed(0, 100);
+    motor.speed(1, 100);
+    delay(1000);
+    motor.stop(1);
+    motor.stop(0);
+    delay(1000);
+    motor.speed(0, -100);
+    motor.speed(1, -100);
+    delay(1000);
+    motor.stop(1);
+    motor.stop(0);
+    delay(1000);
+}
